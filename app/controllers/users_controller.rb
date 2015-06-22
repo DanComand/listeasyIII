52f8a0dcc950fe4905cabc7dfc22a86229b0a408
DataNick <DataNick@users.noreply.github.com>
2015-06-21 20:58:56 -0400

    Added bcrypt, sorcery and sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+
+  def new
+  	@user=@User.new
+  end
+
+  def create
+  	@user=User.new(user_params)
+  	if @user.save
+  		redirect_to @user, notice: "Signed Up!" 
+  	else
+  		render "new"
+  	end
+  end
+
+  def index
+  end
+
+  def show
+  	@user=User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+  	params.requires(:user).permit(:email, :password, :password_confirmation, :name)
+  end
+
+end
